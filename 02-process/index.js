fa029a8b0fcd87bf487c93e80a4048f9437ea2d4
DESKTOP-O5RNJJH\admin <suge@yadingdata.com>
2021-08-09 14:27:02 +0800

    floder delete

--- 02-process/index.js
+++
@@ -1,5 +0,0 @@
-console.log(process.argv.slice(2)) // 命令查看
-console.log(process.env.Path.split(':').join('\n')) //path查看
-console.log(process.arch) // 操作系统位数查看
-console.log(process.platform) // 操作系统查看
-console.log(process.memoryUsage()) // 获取内存使用情况
